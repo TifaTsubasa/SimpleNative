03d3ebe716935542bacb751e5898fcc90115a25c
TifaTsubasa <15922402179@163.com>
2016-04-04 20:28:42 +0800

    Initial Commit

---
+++ SimpleNative/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleNative
+//
+//  Created by 谢许峰 on 16/4/4.
+//  Copyright © 2016年 tifatsubasa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
